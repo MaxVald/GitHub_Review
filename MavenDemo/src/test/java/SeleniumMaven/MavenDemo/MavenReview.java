bb3495f1624159f0994e7b6663192d2446f6539f
maxva <maxva@MaxDell>
2022-11-20 18:54:51 -0500

    Initial Maven Code to GitHub

---
+++ MavenDemo/src/test/java/SeleniumMaven/MavenDemo/MavenReview.java
@@ -0,0 +1,13 @@
+package SeleniumMaven.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class MavenReview {
+	
+	@Test
+	public void MavenClassOne() {
+		System.out.println("let's see if we can print it throuhg maven");
+	}
+	
+
+}
